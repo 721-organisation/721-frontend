b1478de65d8d82d55502fbb1e418629d40a8fe48
Bhav <BPrajapat1@sheffield.ac.uk>
2019-06-05 13:12:22 +0100

    Broken Code

---
+++ app/src/main/java/com/travel721/EventCard.java
@@ -0,0 +1,41 @@
+package com.travel721;
+
+public class EventCard {
+    private String eventImgURL;
+    private String eventName;
+    private String eventLocation;
+
+    public String getEventImgURL() {
+        return eventImgURL;
+    }
+
+    public void setEventImgURL(String eventImgURL) {
+        this.eventImgURL = eventImgURL;
+    }
+
+    public String getEventLocation() {
+        return eventLocation;
+    }
+
+    public void setEventLocation(String eventLocation) {
+        this.eventLocation = eventLocation;
+    }
+
+    public String getEventDate() {
+        return eventDate;
+    }
+
+    public void setEventDate(String eventDate) {
+        this.eventDate = eventDate;
+    }
+
+    private String eventDate;
+    public String getEventName() {
+        return eventName;
+    }
+
+    public void setEventName(String eventName) {
+        this.eventName = eventName;
+    }
+
+}
--- app/src/main/java/com/travel721/MainActivity.java
+++ app/src/main/java/com/travel721/MainActivity.java
@@ -1,13 +1,37 @@
 package com.travel721;
 
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
 import android.os.Bundle;
+import android.view.Window;
+import android.widget.RelativeLayout;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
+
+    private Context context;
+    private ArrayList eventArrayList;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+
+
+        context = MainActivity.this;
+
+        parentView = (RelativeLayout) findViewById(R.id.main_layoutview);
+
+        windowwidth = getWindowManager().getDefaultDisplay().getWidth();
+
+        screenCenter = windowwidth / 2;
+
+        eventArrayList = new ArrayList<>();
+
+        getArrayData();
+
     }
 }
