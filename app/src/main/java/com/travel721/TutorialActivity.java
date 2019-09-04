64f0034c6987dcae5011436e568346b05444eddc
Bhav <BPrajapat1@sheffield.ac.uk>
2019-09-04 22:17:04 +0100

    Created and worked TutorialActivity but not yet worked in to the UX

---
+++ app/src/main/java/com/travel721/TutorialActivity.java
@@ -0,0 +1,79 @@
+package com.travel721;
+
+import android.os.Bundle;
+import android.widget.ImageView;
+
+import androidx.core.content.ContextCompat;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentActivity;
+import androidx.viewpager2.adapter.FragmentStateAdapter;
+import androidx.viewpager2.widget.ViewPager2;
+
+
+public class TutorialActivity extends FragmentActivity {
+    private static final int NUM_PAGES = 3;
+    private ViewPager2 mPager;
+    private FragmentStateAdapter pagerAdapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.tutorial_layout);
+
+        // Instantiate a ViewPager and a PagerAdapter.
+        mPager = findViewById(R.id.view_pager);
+        pagerAdapter = new TutorialSlidePagerAdapter(this);
+        mPager.setAdapter(pagerAdapter);
+        mPager.registerOnPageChangeCallback(new ViewPager2.OnPageChangeCallback() {
+            @Override
+            public void onPageSelected(int position) {
+                super.onPageSelected(position);
+                ImageView progress = findViewById(R.id.tutorial_progress_imageview);
+                switch (position) {
+                    case 0:
+                        progress.setImageDrawable(ContextCompat.getDrawable(getApplicationContext(), R.drawable.ic_loading_1));
+                        break;
+                    case 1:
+                        progress.setImageDrawable(ContextCompat.getDrawable(getApplicationContext(), R.drawable.ic_loading_2));
+                        break;
+                    case 2:
+                        progress.setImageDrawable(ContextCompat.getDrawable(getApplicationContext(), R.drawable.ic_loading_3));
+                        break;
+                }
+            }
+        });
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (mPager.getCurrentItem() == 0) {
+            // If the user is currently looking at the first step, allow the system to handle the
+            // Back button. This calls finish() on this activity and pops the back stack.
+            super.onBackPressed();
+        } else {
+            // Otherwise, select the previous step.
+            mPager.setCurrentItem(mPager.getCurrentItem() - 1);
+        }
+    }
+
+    /**
+     * A simple pager adapter that represents 5 ScreenSlidePageFragment objects, in
+     * sequence.
+     */
+    private class TutorialSlidePagerAdapter extends FragmentStateAdapter {
+
+        public TutorialSlidePagerAdapter(FragmentActivity fa) {
+            super(fa);
+        }
+
+        @Override
+        public Fragment createFragment(int position) {
+            return new TutorialSlideFragment(position);
+        }
+
+        @Override
+        public int getItemCount() {
+            return NUM_PAGES;
+        }
+    }
+}
